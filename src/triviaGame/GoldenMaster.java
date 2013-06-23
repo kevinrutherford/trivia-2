22676e607008d77f155928e2b19f4c2324479391
Kevin Rutherford <kevin@rutherford-software.com>
2013-06-23 11:58:05 +0100

    The golden master now generates itself the first time it is needed

--- src/triviaGame/GoldenMaster.java
+++ src/triviaGame/GoldenMaster.java
@@ -3,6 +3,7 @@ package triviaGame;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintStream;
 
@@ -10,30 +11,36 @@ public class GoldenMaster {
 
 	private static final String MASTER_FILENAME = "./master.txt";
 
-	public static void main(String[] args) throws IOException {
-		GameRunner.runAThousandGames(new PrintStream(MASTER_FILENAME));
-	}
-
 	public String currentValue() {
 		try {
-			return readFileAsString(MASTER_FILENAME);
+			if (!masterFile().exists())
+				generate();
+			return readFileAsString();
 		} catch (IOException e) {
 			return "";
 		}
 	}
+	
+	private void generate() throws FileNotFoundException {
+		GameRunner.runAThousandGames(new PrintStream(MASTER_FILENAME));
+	}
 
-	private static String readFileAsString(String filePath) throws IOException {
-		DataInputStream dis = new DataInputStream(new FileInputStream(filePath));
+	private String readFileAsString() throws IOException {
+		DataInputStream dis = new DataInputStream(new FileInputStream(MASTER_FILENAME));
 		try {
-			long len = new File(filePath).length();
+			long len = masterFile().length();
 			if (len > Integer.MAX_VALUE)
-				throw new IOException("File " + filePath + " too large, was " + len + " bytes.");
+				throw new IOException("File " + MASTER_FILENAME + " too large, was " + len + " bytes.");
 			byte[] bytes = new byte[(int) len];
 			dis.readFully(bytes);
 			return new String(bytes, "UTF-8");
 		} finally {
 			dis.close();
 		}
+	}
+
+	private File masterFile() {
+		return new File(MASTER_FILENAME);
 	}
 
 }
